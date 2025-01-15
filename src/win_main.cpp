237a4a3d4032da801d815c2f79889efaa8bf625d
Luna <154115881+MoonCowGames@users.noreply.github.com>
2025-01-15 19:27:47 +0800

    Adjust format for long-term ease of use.

--- src/win_main.cpp
+++ src/win_main.cpp
@@ -1,84 +1,102 @@
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lparam);
+LRESULT CALLBACK WindowProc(HWND window, 
+                            UINT message, 
+                            WPARAM wParam, 
+                            LPARAM lParam);
 
-int APIENTRY WINAPI WinMain(
-	HINSTANCE instance, HINSTANCE prevInstance, 
-	PSTR cmdLine, int cmdShow)
+int APIENTRY WINAPI WinMain(HINSTANCE instance, 
+                            HINSTANCE prevInstance, 
+                            PSTR cmdLine,
+                            int cmdShow)
 {
-	// Register window class.
+  // Register window class.
 
-	const char CLASS_NAME[] = "Wend Class";
+  const char CLASS_NAME[] = "Wend Class";
 
-	WNDCLASS windowClass = {};
-	windowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-	windowClass.lpfnWndProc = WindowProc; // to impl.
-	windowClass.hInstance = instance;
-	windowClass.lpszClassName = CLASS_NAME;
+  WNDCLASS windowClass = {};
+  windowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  windowClass.lpfnWndProc = WindowProc;
+  windowClass.hInstance = instance;
+  windowClass.lpszClassName = CLASS_NAME;
 
-	RegisterClass(&windowClass);
+  RegisterClass(&windowClass);
 
-	// Create window.
+  // Create window.
 
-	HWND window = CreateWindowExA(
-			0,
-			CLASS_NAME,
-			"Wend Engine",
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			NULL,
-			NULL,
-			instance,
-			NULL
-	);
+  HWND window = CreateWindowExA(
+      0,
+      CLASS_NAME,
+      "Wend Engine",
+      WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      NULL,
+      NULL,
+      instance,
+      NULL
+  );
 
-	if (window == NULL)
-	{
-		return 0;
-	}
+  if (window == NULL)
+  {
+    return 0;
+  }
 
-	ShowWindow(window, cmdShow);
-	UpdateWindow(window);
+  ShowWindow(window, cmdShow);
+  UpdateWindow(window);
 
-	MSG message = {};
-	while (GetMessage(&message, NULL, 0, 0) > 0)
-	{
-		TranslateMessage(&message);
-		DispatchMessage(&message);
-	}
-	return 0;
+  MSG message = {};
+  while (GetMessage(&message, NULL, 0, 0) > 0)
+  {
+    TranslateMessage(&message);
+    DispatchMessage(&message);
+  }
+  return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch (message)
-	{
-		case WM_CLOSE:
-			if (MessageBox(window, "Are you sure you want to quit? Unsaved progress will be lost.", "Wend", MB_OKCANCEL) == IDOK)
-			{
-				DestroyWindow(window);
-			}
-			return 0;
+  switch (message)
+  {
+    case WM_CREATE:
+    {
+      return 0;
+    }
+
+    case WM_ACTIVATEAPP:
+    {
+      return 0;
+    }
+
+    case WM_CLOSE:
+    {
+      if (MessageBox(window, "Are you sure you want to quit? Unsaved progress will be lost.", "Wend", MB_OKCANCEL) == IDOK)
+      {
+        DestroyWindow(window);
+      }
+      return 0;
+    }
 
     case WM_DESTROY:
-			PostQuitMessage(0);
-			return 0;
+    {
+      PostQuitMessage(0);
+      return 0;
+    }
 
     case WM_PAINT:
-		{
-			PAINTSTRUCT painter;
-			HDC deviceContext = BeginPaint(window, &painter);
+    {
+      PAINTSTRUCT painter;
+      HDC deviceContext = BeginPaint(window, &painter);
 
-			// All painting occurs here, between BeginPaint and EndPaint.
+      // All painting occurs here, between BeginPaint and EndPaint.
 
-			FillRect(deviceContext, &painter.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+      FillRect(deviceContext, &painter.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
 
-			EndPaint(window, &painter);
-			return 0;
-		}
-	}
-	return DefWindowProc(window, message, wParam, lParam);
+      EndPaint(window, &painter);
+      return 0;
+    }
+  }
+  return DefWindowProc(window, message, wParam, lParam);
 }
